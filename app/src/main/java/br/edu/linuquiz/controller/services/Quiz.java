6a7ba3b688a7b5afad3b4555bdf1da526ed1fc15
lik3a <lik3games@gmail.com>
2023-01-02 23:05:40 -0400

    Logica Experimental Adicionada

--- app/src/main/java/br/edu/linuquiz/controller/services/Quiz.java
+++ app/src/main/java/br/edu/linuquiz/controller/services/Quiz.java
@@ -4,13 +4,58 @@ import java.util.HashMap;
 import br.edu.linuquiz.R;
 import android.content.res.Resources;
 
-public class Quiz {
-    HashMap<String, HashMap<String, String>> OS = new HashMap<>();
+import java.util.Random;
 
-    public static Quiz instance = new Quiz();
-    private Quiz(){
-        for(String name : Resources.getSystem().getStringArray(R.array.profileIcon)){
-            OS.put(name, new HashMap<>());
+public class Quiz{
+    Resources r;
+    HashMap<String, HashMap<String, String>> OS = new HashMap<>();
+    HashMap<String, String> arch = new HashMap<>();
+    HashMap<String, String> debian = new HashMap<>();
+    HashMap<String, String> fedora = new HashMap<>();
+    HashMap<String, String> tux = new HashMap<>();
+
+    String[] formats = new String[10];
+
+    public static Quiz getInstance(String[] OS){
+        return new Quiz(OS);
+    }
+    private Quiz(String[] Systems){
+        formats[0] = "Qual é o formato padrão dos pacotes? ";
+        formats[1] = "Qual é o Package Manager? ";
+        formats[9] = "Qual é o bootloader padrão? ";
+
+        arch.put(formats[1], "pacman");
+        arch.put(formats[0], "x86_64");
+        debian.put(formats[1], "dpkg");
+        debian.put(formats[0], ".deb");
+        fedora.put(formats[1], "dnf");
+        fedora.put(formats[0], "rpm");
+
+        tux.put(formats[9], "grub");
+
+        for(String name : Systems) {
+            switch(name) {
+                case "Arch":
+                    OS.put(name, arch);
+                    break;
+                case "Debian":
+                    OS.put(name, debian);
+                    break;
+                case "Fedora":
+                    OS.put(name, fedora);
+                    break;
+                default:
+                    OS.put(name, tux);
+                    break;
+            }
         }
+    }
+    public String requestQuest(String OS_name){
+        int i = new Random().nextInt(2);
+        return formats[i];
+    }
+
+    public String requestAnswer(String OS_name, String question){
+        return OS.get(OS_name).get(question);
     }
 }
--- app/src/main/java/br/edu/linuquiz/view/MenuActivity.java
+++ app/src/main/java/br/edu/linuquiz/view/MenuActivity.java
@@ -3,12 +3,43 @@ package br.edu.linuquiz.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import br.edu.linuquiz.R;
+import br.edu.linuquiz.controller.services.Quiz;
 
 public class MenuActivity extends AppCompatActivity {
+    EditText answer;
+    TextView question;
+    Button confirm;
 
+    String quest;
+    static Quiz instance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        instance = Quiz.getInstance(getResources().getStringArray(R.array.profileIcon));
 
+        answer = findViewById(R.id.edtAnswer);
+        question = findViewById(R.id.txtQuest);
+        confirm = findViewById(R.id.btnConfirm);
+
+        question.setText(instance.requestQuest("Arch"));
+
+        confirm.setOnClickListener(l -> {
+            if (answer.getText().toString().trim()
+                    .equals(instance.requestAnswer("Arch", question.getText().toString()))){
+
+                Toast.makeText(getApplicationContext(), "certo", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "errado", Toast.LENGTH_SHORT).show();
+            }
+            question.setText(instance.requestQuest("Arch"));
+        });
     }
 }
